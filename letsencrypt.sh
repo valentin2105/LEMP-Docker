7c99f9114859f53d0a06f0a671b3c681cb9d7448
Valentin Ouvrard <valentin210594@gmail.com>
2016-04-12 09:05:11 +1100

    deploy tls on same folder

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,4 +1,4 @@
 docker run -it --rm -p 443:443 -p 80:80 --name letsencrypt \
-            -v "/etc/letsencrypt:/etc/letsencrypt" \
-            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            -v "./etc/letsencrypt:/etc/letsencrypt" \
+            -v "./var/lib/letsencrypt:/var/lib/letsencrypt" \
             quay.io/letsencrypt/letsencrypt:latest auth
