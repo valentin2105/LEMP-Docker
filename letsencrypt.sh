40f60eda830473dec24fb9872a1c16bc5bfc0ac5
valentin2105 <valentin@ouvrard.it>
2016-04-11 03:45:06 +0000

    letsencrypt docker cmd

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,32 +1,4 @@
-web_db:
-   image: mariadb:latest
-   restart: always
-   volumes:
-    - ./var/mysql:/var/lib/mysql
-   environment:
-    MYSQL_ROOT_PASSWORD: Abracadabraaa01
-
-web_web:
-  image: nginx
-  restart: always
-  ports:
-    - 80:80
-    - 443:443
-  log_driver: syslog
-  links:
-    - web_fpm
-  volumes:
-    - ./www:/var/www/html:rw
-    - ./etc/nginx/nginx.conf:/etc/nginx/nginx.conf:ro
-    - ./var/log/nginx:/var/log/nginx
-    - ./etc/letsencrypt:/etc/letsencrypt
-    - ./etc/nginx/certs/dhparam.pem:/etc/nginx/certs/dhparam.pem
-
-web_fpm:
-  build: ./PHP-FPM/
-  image: fpm-7.0.5
-  restart: always
-  links:
-    - web_db:mysql
-  volumes:
-    - ./www:/var/www/html
+docker run -it --rm -p 443:443 -p 80:80 --name letsencrypt \
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            quay.io/letsencrypt/letsencrypt:latest auth
